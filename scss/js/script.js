0851086b0c733abddcab9a1d3281d43dd92edc82
UlviNuri <102859099+UlviNuri@users.noreply.github.com>
2022-05-28 15:09:40 +0400

    new

--- scss/js/script.js
+++ scss/js/script.js
@@ -1,12 +1,39 @@
-var sum=0;
-for (let i=0; i<=100; i++) {
-    let total=0;
-    if (i%5==0) {
-        total+=i
-        console.log(i)
-       
-        
-    }
-}
+// var sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//     let total = 0;
+//     if (i % 5 == 0) {
+//         total += i
+//         console.log(i);
+//     }
+//     sum += total
+// }
 
-console.log(i)
+
+
+
+// 3 e bolunenler
+
+//  var sum = 0;{}
+// for (let i = 3; i <= 100; i++) {
+//     let total = 0;
+//     if (i % 3 == 0) {
+//         total += i
+//         console.log(i);
+//     }
+//     sum += total
+// }
+
+
+// const arry=  [ "H", "E","L", "L","O"]
+
+// for (let index = 0; index < arry.length; index++) {
+
+    
+// }
+// console.log(arry)
+
+
+function ad (a,b){
+    return a*b 
+}
+console.log(ad(3,4));
